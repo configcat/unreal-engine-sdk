f9ca35db503de9954620dd26cb6c016164f34a4a
Oprea Alexandru <oprea_alexandrum@yahoo.com>
2024-05-15 10:06:43 +0300

    separate module for wrappers

---
+++ Source/ConfigCatWrappers/Private/ConfigCatWrappers.cpp
@@ -0,0 +1,17 @@
+﻿#include "ConfigCatWrappers.h"
+
+#define LOCTEXT_NAMESPACE "FConfigCatWrappersModule"
+
+void FConfigCatWrappersModule::StartupModule()
+{
+    
+}
+
+void FConfigCatWrappersModule::ShutdownModule()
+{
+    
+}
+
+#undef LOCTEXT_NAMESPACE
+    
+IMPLEMENT_MODULE(FConfigCatWrappersModule, ConfigCatWrappers)
