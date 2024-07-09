abb8b73b7ec97f683b89c2a88e64fe38238ab014
Oprea Alexandru <oprea_alexandrum@yahoo.com>
2024-07-09 09:39:27 +0300

    sha hashing implementation using Unreal's functions

---
+++ Source/ConfigCat/Private/ConfigCatSha.cpp
@@ -0,0 +1,27 @@
+﻿// Copyright (c) ConfigCat 2024. All Rights Reserved.
+
+#include "ConfigCatSha.h"
+
+std::string configcat::sha1(const std::string& input)
+{
+	const FString InputString = UTF8_TO_TCHAR(input.c_str());
+
+	FSHAHash const Hash = FSHA1::HashBuffer(*InputString, InputString.Len());
+	FString FinalString;
+	for (int i = 0; i < 20; i++)
+	{
+		FinalString += FString::Printf(TEXT("%02x"), Hash.Hash[i]);
+	}
+
+	return TCHAR_TO_UTF8(*FinalString);
+}
+
+std::string configcat::sha256(const std::string& input)
+{
+	const FString InputString = UTF8_TO_TCHAR(input.c_str());
+
+	FSHA256Signature Hash;
+	FPlatformMisc::GetSHA256Signature(*InputString, InputString.Len(), Hash);
+
+	return TCHAR_TO_UTF8(*Hash.ToString());
+}
