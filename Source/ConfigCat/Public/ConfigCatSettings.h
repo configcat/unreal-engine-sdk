a6959a94c661576f59a7f5a8f815a4d27618c013
Oprea Alexandru <oprea_alexandrum@yahoo.com>
2024-08-05 18:55:14 +0300

    removed qualifiers from tooltips

--- Source/ConfigCat/Public/ConfigCatSettings.h
+++ Source/ConfigCat/Public/ConfigCatSettings.h
@@ -60,75 +60,75 @@ class CONFIGCAT_API UConfigCatSettings : public UDeveloperSettings
 
 public:
 	/**
-	 * @brief SDK Key to access your feature flags and configurations. Get it from ConfigCat Dashboard.
+	 * SDK Key to access your feature flags and configurations. Get it from ConfigCat Dashboard.
 	 */
 	UPROPERTY(Config, EditAnywhere, Category = "ConfigCat|Integration")
 	FString SdkKey = TEXT("");
 	/**
-	 * @brief The base ConfigCat CDN url.
+	 * The base ConfigCat CDN url.
 	 */
 	UPROPERTY(Config, EditAnywhere, Category = "ConfigCat|Client")
 	FString BaseUrl = TEXT("");
 	/**
-	 * @brief Set this parameter to be in sync with the Data Governance preference on the [Dashboard](https://app.configcat.com/organization/data-governance).
-	 * @note Only Organization Admins have access
+	 * Set this parameter to be in sync with the Data Governance preference on the [Dashboard](https://app.configcat.com/organization/data-governance).
+	 * Only Organization Admins have access
 	 */
 	UPROPERTY(Config, EditAnywhere, Category = "ConfigCat|Client")
 	EDataGovernance DataGovernance = EDataGovernance::Global;
 	/**
-	 * @brief The number of milliseconds to wait for the server to make the initial connection
-	 * @note 0 means it never times out during transfer
+	 * The number of milliseconds to wait for the server to make the initial connection
+	 * 0 means it never times out during transfer
 	 */
 	UPROPERTY(Config, EditAnywhere, Category = "ConfigCat|Client", meta = (DisplayName = "Connection Timeout (in milliseconds)", ClampMin = "0", UIMin = "0", ForceUnits = "ms"))
 	uint32 ConnectionTimeoutMs = 8000;
 	/**
-	 * @brief The number of milliseconds to wait for the server to respond before giving up
-	 * @note 0 means it never times out during transfer
+	 * The number of milliseconds to wait for the server to respond before giving up
+	 * 0 means it never times out during transfer
 	 */
 	UPROPERTY(Config, EditAnywhere, Category = "ConfigCat|Client", meta = (DisplayName = "Read Timeout (in milliseconds)", ClampMin = "0", UIMin = "0", ForceUnits = "ms"))
 	uint32 ReadTimeoutMs = 5000;
 	/**
-	 * @brief The polling mode.
+	 * The polling mode.
 	 */
 	UPROPERTY(Config, EditAnywhere, Category = "ConfigCat|Client")
 	EPollingMode PollingMode;
 	/**
-	 * @brief Sets at least how often this policy should fetch the latest configuration and refresh the cache.
+	 * Sets at least how often this policy should fetch the latest configuration and refresh the cache.
 	 */
 	UPROPERTY(Config, EditAnywhere, Category = "ConfigCat|Client", meta = (DisplayName = "Auto Poll Interval (in seconds)", EditCondition = "PollingMode == EPollingMode::Auto", EditConditionHides, ClampMin = "0", UIMin = "0", ForceUnits = "s"))
 	uint32 AutoPollIntervalSeconds = 60;
 	/**
-	 * @brief Sets the maximum waiting time between initialization and the first config acquisition in seconds.
+	 * Sets the maximum waiting time between initialization and the first config acquisition in seconds.
 	 */
 	UPROPERTY(Config, EditAnywhere, Category = "ConfigCat|Client", meta = (DisplayName = "Maximum Inititial Wait Time (in seconds)", EditCondition = "PollingMode == EPollingMode::Auto", EditConditionHides, ClampMin = "0", UIMin = "0", ForceUnits = "s"))
 	uint32 MaxInitWaitTimeSeconds = 5;
 	/**
-	 * @brief Sets how long the cache will store its value before fetching the latest from the network again.
+	 * Sets how long the cache will store its value before fetching the latest from the network again.
 	 */
 	UPROPERTY(Config, EditAnywhere, Category = "ConfigCat|Client", meta = (DisplayName = "Cache Refresh Interval (in seconds)", EditCondition = "PollingMode == EPollingMode::LazyLoad", EditConditionHides, ClampMin = "0", UIMin = "0", ForceUnits = "s"))
 	uint32 CacheRefreshIntervalSeconds = 60;
 	/**
-	 * @brief Proxy addresses. e.g. { "https": "your_proxy_ip:your_proxy_port" }
+	 * Proxy addresses. e.g. { "https": "your_proxy_ip:your_proxy_port" }
 	 */
 	UPROPERTY(Config, EditAnywhere, Category = "ConfigCat|Proxy")
 	TMap<FString, FString> Proxies;
 	/**
-	 * @brief Proxy authentication.
+	 * Proxy authentication.
 	 */
 	UPROPERTY(Config, EditAnywhere, Category = "ConfigCat|Proxy")
 	TMap<FString, FProxyAuthentication> ProxyAuthentications;
 	/**
-	 * @brief Indicates whether the SDK should be initialized in offline mode or not.
+	 * Indicates whether the SDK should be initialized in offline mode or not.
 	 */
 	UPROPERTY(Config, EditAnywhere, Category = "ConfigCat|Client")
 	bool bStartOffline;
 	/**
-	 * @brief Specifies the data source for overrides if any
+	 * Specifies the data source for overrides if any
 	 */
 	UPROPERTY(Config, EditAnywhere, Category= "ConfigCat|Override")
 	EOverrideMode OverrideMode;
 	/**
-	 * @brief Specifies how the overrides should apply over the downloaded values
+	 * Specifies how the overrides should apply over the downloaded values
 	 */
 	UPROPERTY(Config, EditAnywhere, Category= "ConfigCat|Override", meta = (EditCondition = "OverrideMode != EOverrideMode::None", EditConditionHides))
 	EOverrideBehaviour OverrideBehaviour;
