dca0dedfab89119a9536dbe47bb8f62be79baec2
Oprea Alexandru <oprea_alexandrum@yahoo.com>
2025-08-20 08:30:06 +0300

    added missing include

--- Source/ConfigCat/Private/ConfigCatSubsystem.cpp
+++ Source/ConfigCat/Private/ConfigCatSubsystem.cpp
@@ -6,6 +6,7 @@
 #include <ConfigCatCppSDK/Include/configcatclient.h>
 #include <ConfigCatCppSDK/Include/fileoverridedatasource.h>
 #include <ConfigCatCppSDK/Include/mapoverridedatasource.h>
+#include <Async/TaskGraphInterfaces.h>
 #include <HAL/FileManager.h>
 #include <HAL/PlatformFileManager.h>
 #include <Kismet/GameplayStatics.h>
